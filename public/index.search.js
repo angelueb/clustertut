6e0e0272aca14d04af151d15d37015dae1e619c6
angelueb <angelblancovhir@gmail.com>
2023-02-26 18:28:55 +0100

    to gh-pages_06

---
+++ public/index.search.js
@@ -0,0 +1,51 @@
+var relearn_search_index = [
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Basics",
+    "uri": "/clustertut/basics/index.html"
+  },
+  {
+    "content": "First Content Discover what this Hugo theme is all about and the core concepts behind it, FIRST CONTACTT\n",
+    "description": "",
+    "tags": null,
+    "title": "First Content",
+    "uri": "/clustertut/basics/first-content/index.html"
+  },
+  {
+    "content": "Lorem Ipsum.\n",
+    "description": "",
+    "tags": null,
+    "title": "Seconds",
+    "uri": "/clustertut/seconds/index.html"
+  },
+  {
+    "content": "Second content Discover what this Hugo theme is all about and the core concepts behind it, with this SECOND CONTENT\n",
+    "description": "",
+    "tags": null,
+    "title": "Second Content",
+    "uri": "/clustertut/basics/second-content/index.html"
+  },
+  {
+    "content": "This tutorial covers the basics about running computing jobs on the Statistics and Bioinformatics Unit cluster, using some practical examples.\nYou can navigate using the left navbar or the arrow buttons on the upper left corner.\n",
+    "description": "",
+    "tags": null,
+    "title": "UEB Cluster Basic Tutorial",
+    "uri": "/clustertut/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/clustertut/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/clustertut/tags/index.html"
+  }
+]
